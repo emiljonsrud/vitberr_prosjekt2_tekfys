329e43a3ebb6d9ccfa3ce031d23546db29ac6ea8
sundematias <matias.sunde@gmail.com>
2022-03-14 03:43:31 +0100

    tettttst

--- animation.py
+++ animation.py
@@ -16,8 +16,6 @@ som inneholder lastens posisjon relativt metasenteret. Lasten vil da animeres so
 '''
 
 
-
-
 # M = metasenteret = midt paa dekk
 # C = skipets tyngdepunkt
 
@@ -25,21 +23,23 @@ som inneholder lastens posisjon relativt metasenteret. Lasten vil da animeres so
 R = 10  # skipets radius (m)
 h = 4 * R / (3 * np.pi)  # avstand M - C
 
+
 def init_anim():
     """ Initialises the animation.
     """
 
     global ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
     boat, = plt.plot([], [],
-                    color="k", linewidth=1)
+                     color="k", linewidth=1)
     deck, = plt.plot([], [], color="k", linewidth=1)
-    sea_surface, = plt.plot([-R*10, R* 10], [0, 0], color='blue', linewidth=2)  # The surface
+    sea_surface, = plt.plot([-R*10, R * 10], [0, 0],
+                            color='blue', linewidth=2)  # The surface
     last, = plt.plot([], [], color="r", marker="o", markersize=10)
     CM, = plt.plot([], [], color="g", marker="o", markersize=10)
     venstre_gjerde, = plt.plot([], [], color="k", marker="|", markersize=25)
     høyre_gjerde, = plt.plot([], [], color="k", marker="|", markersize=25)
-    ax.set_xlim([-R*1.3, R* 1.3])
-    ax.set_ylim([-R*1.1, R* 1.1])
+    ax.set_xlim([-R*1.3, R * 1.3])
+    ax.set_ylim([-R*1.1, R * 1.1])
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     ax.set_aspect("equal")
@@ -47,13 +47,13 @@ def init_anim():
     textbox_theory = ax.text(0.775, 0.95, '', transform=ax.transAxes, fontsize=12,
                              verticalalignment='top', bbox=props)
 
-    return ax, boat, deck, last, CM,venstre_gjerde,høyre_gjerde, textbox_theory
+    return ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
 
 
 def animate(M, theta, t, x_C, y_C, s_L, gjerde=False):
     global ax, boat, deck, last, CM, venstre_gjerde, høyre_gjerde, textbox_theory
     ax.set_xlim([-R * 1.1 + np.amin(x_C), R * 1.1 + np.amax(x_C)])
-    ax.set_ylim([-R*1.1, R* 1.1])
+    ax.set_ylim([-R*1.1, R * 1.1])
     angle_values = np.linspace(0, np.pi, 100)
     metasenter_x = x_C[M] - h * np.sin(theta[M])
     metasenter_y = y_C[M] + h * np.cos(theta[M])
@@ -61,14 +61,16 @@ def animate(M, theta, t, x_C, y_C, s_L, gjerde=False):
     ys = R * np.sin(angle_values + np.pi + theta[M]) + metasenter_y
     boat.set_data(xs, ys)
     deck.set_data([xs[0], xs[-1]], [ys[0], ys[-1]])
-    if s_L[M] !=-42:
+    if s_L[M] != -42:
         last.set_data(metasenter_x + s_L[M] * np.cos(theta[M]),
                       metasenter_y + s_L[M] * np.sin(theta[M]))
     CM.set_data(x_C[M], y_C[M])
     if gjerde:
-        venstre_gjerde.set_data([metasenter_x - R * np.cos(theta[M])], [metasenter_y - R * np.sin(theta[M])])
-        høyre_gjerde.set_data([metasenter_x + R * np.cos(theta[M])], [metasenter_y + R * np.sin(theta[M])])
-    theta_string= r'$\theta = %.2f$' % (theta[M] * 180 / np.pi) + r"$\degree$"
+        venstre_gjerde.set_data(
+            [metasenter_x - R * np.cos(theta[M])], [metasenter_y - R * np.sin(theta[M])])
+        høyre_gjerde.set_data(
+            [metasenter_x + R * np.cos(theta[M])], [metasenter_y + R * np.sin(theta[M])])
+    theta_string = r'$\theta = %.2f$' % (theta[M] * 180 / np.pi) + r"$\degree$"
     time_string = '$t =  %.2f$' % (t[M])
     textbox_theory.set_text(
         theta_string + '\n' + time_string)
@@ -107,8 +109,9 @@ def animate_deck_movement(t, theta, x_C, y_C, s_L=[], gjerde=False, stepsize=0.0
                                      fargs=(theta_anim, t_anim, x_C_anim, y_C_anim, s_L_anim, gjerde))
     plt.show()
 
-simulation_data = np.load('simulation_data/2c.npz')
-w_num, t_num = simulation_data['w_num'], simulation_data['t_num']
+
+simulation_data = np.load('simulation_data/2d.npz')
+w_num, t_num = simulation_data['w_num_2'], simulation_data['t_num_2']
 y_num_rk4 = w_num[3]
 omega_num_rk = w_num[1]
 theta_num_rk4 = w_num[0]
@@ -119,4 +122,5 @@ s_L_num = w_num[6]
 print(len(w_num))
 h_step = 0.01
 print(t_num)
-animate_deck_movement(t_num, theta_num_rk4, x_C_num_rk4 , y_C_num_rk4, stepsize=h_step, s_L=s_L_num,gjerde=True)
+animate_deck_movement(t_num, theta_num_rk4, x_C_num_rk4,
+                      y_C_num_rk4, stepsize=h_step, s_L=s_L_num, gjerde=True)
